8b7ba8763d94f62248ba9f7f6af449f857c8bac1
kishan <kishangupta3003@gmail.com>
2025-09-16 16:53:15 +0530

    Add ExclusiveOffers, StarRating, and Testimonial components to enhance the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination.jsx'
+import ExclusiveOffers from '../components/ExclusiveOffers.jsx'
+import Testimonial from '../components/Testimonial.jsx'
 
 const Home = () => {
   return (
     <>
         <Hero/>
         <FeaturedDestination />
+        <ExclusiveOffers/>
+        <Testimonial/>
     </>
   )
 }
